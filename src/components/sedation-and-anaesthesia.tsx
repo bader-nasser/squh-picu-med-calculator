7ba6fbf0647ebeff0255424beda877c833cb669e
Bader Nasser <bader4tech@gmail.com>
2023-06-17 22:56:04 +0400

    Add Morphine to data

--- src/components/sedation-and-anaesthesia.tsx
+++ src/components/sedation-and-anaesthesia.tsx
@@ -8,6 +8,26 @@ const {Title} = Typography;
 // @ts-expect-error Silencing a strange error!
 const {medications}: {medications: SedationAndAnaesthesiaDataType[]} = data;
 
+type DoseAmountArgs = {
+	multiplier: number;
+	weight: number;
+	divider?: number;
+};
+
+function getDoseAmount({multiplier, weight, divider = 1}: DoseAmountArgs) {
+	return getNumberWithOneDecimalPoint(multiplier * weight / divider);
+}
+
+function getNsAmount({multiplier, weight, divider = 1}: DoseAmountArgs) {
+	return getNumberWithOneDecimalPoint(50 - (multiplier * weight / divider));
+}
+
+type FormulaObjectData = {
+	text: string;
+	multiplier: number | number[];
+	divider: number;
+};
+
 type SedationAndAnaesthesiaDataType = {
 	name: string;
 	dose:
@@ -17,7 +37,7 @@ type SedationAndAnaesthesiaDataType = {
 		multiplier: number | number[];
 		unit: string;
 	};
-	formula_50ml: string | Record<string, string>;
+	formula_50ml: string | Record<string, string> | Record<string, FormulaObjectData>;
 	compatible: string[];
 	incompatible: string[];
 };
@@ -49,12 +69,10 @@ const columns: ColumnsType<DataType> = [
 			const {info, multiplier, unit} = dose;
 			let computedValue = '';
 			if (typeof multiplier === 'number') {
-				computedValue = `${getNumberWithOneDecimalPoint(
-					multiplier * weight,
-				)}`;
+				computedValue = `${getDoseAmount({multiplier, weight})}`;
 			} else {
 				const [v1, v2] = multiplier.map(m =>
-					getNumberWithOneDecimalPoint(m * weight),
+					getDoseAmount({multiplier: m, weight}),
 				);
 				computedValue = `${v1} - ${v2}`;
 			}
@@ -73,8 +91,43 @@ const columns: ColumnsType<DataType> = [
 		title: 'Formula (50ml)',
 		dataIndex: 'formula_50ml',
 		key: 'formula_50ml',
-		render(_, {formula_50ml}) {
-			return displayData(formula_50ml, {capitalize: true});
+		render(_, {formula_50ml, weight}) {
+			if (typeof formula_50ml === 'string') {
+				return displayData(formula_50ml, {capitalize: true});
+			}
+
+			const newFormula: Record<string, string> = {};
+			for (const [key, value] of Object.entries(formula_50ml)) {
+				if (typeof value === 'string') {
+					newFormula[key] = value;
+				} else {
+					// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+					const newValue: FormulaObjectData = value;
+					const {text, multiplier, divider} = newValue;
+					let newText = text;
+					let amount: string;
+					let amountMl: string;
+					if (typeof multiplier === 'number') {
+						amount = getDoseAmount({multiplier, weight}).toString();
+						amountMl = getDoseAmount({multiplier, weight, divider}).toString();
+						const nsAmount = getNsAmount({multiplier, weight, divider}).toString();
+						newText = newText.replace('_ns_amount_', nsAmount);
+					} else {
+						const [v1, v2] = multiplier.map(m =>
+							getDoseAmount({multiplier: m, weight}));
+						const [v1Ml, v2Ml] = multiplier.map(m =>
+							getDoseAmount({multiplier: m, weight, divider}));
+						amount = `(${v1} to ${v2})`;
+						amountMl = `(${v1Ml} to ${v2Ml})`;
+					}
+
+					newText = newText.replace('_amount_', amount);
+					newText = newText.replace('_amount_ml_', amountMl);
+					newFormula[key] = newText;
+				}
+			}
+
+			return displayData(newFormula, {capitalize: true});
 		},
 	},
 	{
